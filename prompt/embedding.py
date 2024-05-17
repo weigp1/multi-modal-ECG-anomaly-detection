e97e62fa4fd335b46c016042c3111b9e4862f9e2
ZhangCWei <zhangchw9@mail2.sysu.edu.cn>
2024-05-17 15:04:50 +0800

    update 0517

--- prompt/embedding.py
+++ prompt/embedding.py
@@ -1,17 +1,19 @@
 from sentence_transformers import SentenceTransformer, util
 
-# 加载预训练的模型（例如bert-base-nli-mean-tokens）
-model = SentenceTransformer('bert-base-nli-mean-tokens')
 
-# 输入文本
-text1 = "这是一段示例文本。"
-text2 = "这是另一段示例文本。"
+def encode(text):
+    # 加载预训练的模型
+    model = SentenceTransformer('sentence-t5-base')
 
-# 计算文本的嵌入
-embeddings1 = model.encode(text1, convert_to_tensor=True)
-embeddings2 = model.encode(text2, convert_to_tensor=True)
+    # 计算文本的嵌入
+    embedding = model.encode(text, convert_to_tensor=True)
 
-# 计算两个文本之间的余弦相似度
-cosine_similarity = util.pytorch_cos_sim(embeddings1, embeddings2)
+    return embedding
 
-print(f"文本1和文本2之间的余弦相似度：{cosine_similarity.item()}")
+
+def similarity(embedding1, embedding2):
+    # 计算两个文本之间的余弦相似度
+    cosine_similarity = util.pytorch_cos_sim(embedding1, embedding2)
+
+    return cosine_similarity.item()
+
