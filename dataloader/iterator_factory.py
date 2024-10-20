8a8aae27636dc576241f83f187a67f6b3375a7c2
weigp1 <132802177+weigp1@users.noreply.github.com>
2024-10-20 23:07:43 +0800

    Update iterator_factory.py

--- dataloader/iterator_factory.py
+++ dataloader/iterator_factory.py
@@ -1,10 +1,10 @@
 import torch.utils.data
-from dataloader.data_loader import DataIter, worker_init_fn
+from dataloader.data_loader import DataIter
 
-def get_dataiter(root, set, batch_size, num_worker, mod='normal'):
+def get_dataiter(root, set, batch_size, num_worker, mod='clip'):
     assert mod == 'clip', '不支持的mod'
 
     data_iter = DataIter(root, set)
-    data_loader = torch.utils.data.DataLoader(data_iter, batch_size=batch_size, num_workers=num_worker, pin_memory=True, worker_init_fn=worker_init_fn)
+    data_loader = torch.utils.data.DataLoader(data_iter, batch_size=batch_size, pin_memory=True)
 
     return data_loader, data_iter
